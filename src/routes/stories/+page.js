0de96a32172b56b4d57f665da8177fdf6acda392
OvenMagnetron <jessekramer15@gmail.com>
2024-12-03 21:30:05 +0100

    JK - Opzet voor gradient

--- src/routes/stories/+page.js
+++ src/routes/stories/+page.js
@@ -1,7 +1,7 @@
 /** @type {import('./$types').PageLoad} */
 export let csr = true;
 
-import { fetchAllData, mapStoriesWithDetails } from '$lib/api';
+import { fetchAllData, mapStoriesWithDetails, mapPlaylistsWithDetails } from '$lib/api';
 
 export async function load({ fetch }) {
     const data = await fetchAllData(fetch);
@@ -9,8 +9,12 @@ export async function load({ fetch }) {
     // Enrich stories with details
     const storiesWithDetails = mapStoriesWithDetails(data.stories, data.audios, data.languages);
 
+    // Enrich stories with details
+    const enrichedPlaylists = mapPlaylistsWithDetails(data.playlists, data.stories, data.playlistStories);
+
     return {
         ...data,
-        stories: storiesWithDetails // Stories now include corresponding audios and languages
+        stories: storiesWithDetails, // Stories now include corresponding audios and languages
+        playlists: enrichedPlaylists // Playlists now include enriched details like playtime
     };
 }
--- src/routes/stories/+page.svelte
+++ src/routes/stories/+page.svelte
@@ -1,24 +1,56 @@
 <script>
     import Story from '../../components/layout/story.svelte';
+    import Playlist from '../../components/layout/playlist.svelte';
 
     /** @type {import('./$types').PageData} */
     export let data;
 </script>
-
-<section>
-    <h2>Stories</h2>
-    <ul>
-        {#each data.stories as story}
-            <Story {story} />
-        {/each}
-    </ul>
-</section>
+<main>
+    <section>
+        <h2>Stories</h2>
+        <ul>
+            {#each data.stories as story}
+                <Story {story} />
+            {/each}
+        </ul>
+    </section>
+    <section>
+        <h2>Playlists</h2>
+        <ul>
+            {#each data.playlists as playlist}
+                <Playlist {playlist} />
+            {/each}
+        </ul>
+      </section>
+      <section>
+        <h2>Stories</h2>
+        <ul>
+            {#each data.stories as story}
+                <Story {story} />
+            {/each}
+        </ul>
+    </section>
+    <section>
+        <h2>Playlists</h2>
+        <ul>
+            {#each data.playlists as playlist}
+                <Playlist {playlist} />
+            {/each}
+        </ul>
+      </section>
+</main>
 
 <style>
-    section {
-        background-color: purple;
-        padding: 20px;
+    main {
+        background: rgb(20,33,81);
+        background: linear-gradient(0deg, rgba(20,33,81,1) 0%, rgba(71,24,113,1) 100%);
+        padding: 1.2em;
         color: white;
+        height: 100%;
+    }
+
+    section {
+        margin: 2.5em 0 2.5em 0;
     }
 
     ul {
